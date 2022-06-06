0ded609ddc49186e2f21277972569958e5857a45
Maksym Panchenko <maksym.panchenko@requestum.com>
2022-06-06 23:32:48 +0300

    add: users page

---
+++ src/app/modules/users/users.component.ts
@@ -0,0 +1,46 @@
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {User} from "@models/interfaces/user.interface";
+import {MatPaginator} from "@angular/material/paginator";
+
+@Component({
+  selector: 'users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit, AfterViewInit {
+  startUser: number;
+  numberOfUsers: number = 3;
+
+  users: User[];
+  showedUsers: User[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        this.users = json;
+        this.showUsers()
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.paginator.page
+      .subscribe(() => {
+        this.showUsers()
+      });
+  }
+
+  showUsers() {
+    if (this.paginator && this.users?.length) {
+      this.startUser = this.paginator.pageIndex * this.paginator.pageSize + 1;
+      this.showedUsers = this.users.filter(e => e.id >= this.startUser && e.id < (this.startUser + this.paginator.pageSize));
+    }
+  }
+}
