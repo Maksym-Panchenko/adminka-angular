5d9e18f06df2b03bf025cdf3376b024c7d12a114
Maksym Panchenko <maksym.panchenko@requestum.com>
2022-06-09 17:42:32 +0300

    add: modals, create and edit post markup with API, post comments page

--- src/app/common/modules/forms/forms.module.ts
+++ src/app/common/modules/forms/forms.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from './mat-form-field/mat-form-field.module';
 import {MatFormCheckboxModule} from "./mat-form-checkbox/mat-form-checkbox.module";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormTextareaModule} from "./mat-form-textarea/mat-form-textarea.module";
 
 @NgModule({
   exports: [
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatFormCheckboxModule
+    MatFormCheckboxModule,
+    MatFormTextareaModule
   ]
 })
 export class AppFormsModule {}
