5b551bc55824ea713489a4cea51598010d80dc3a
Maksym Panchenko <maksym.panchenko@requestum.com>
2022-06-13 13:43:04 +0300

    update: single-posts components

--- src/app/modules/single-post/single-post.component.ts
+++ src/app/modules/single-post/single-post.component.ts
@@ -10,6 +10,10 @@ import {MatDialog} from "@angular/material/dialog";
 import {EntityModalType} from "@models/enums/entity-modal-type";
 import {ModeType} from "@models/enums/mode-type";
 import { Location } from '@angular/common'
+import {MessageDialogComponent} from "../../common/modules/modals/message-dialog/message-dialog.component";
+import {MessageModalType} from "@models/enums/message-modal-type.enum";
+import {IMessageModal} from "@models/interfaces/modal/message-modal.inteface";
+import {filter} from "rxjs/operators";
 
 
 @Component({
@@ -46,20 +50,14 @@ export class SinglePostComponent implements OnInit {
     this._postApi.getItem(this.postId).subscribe((post) => {
       this.post = post;
       this.isLoadingPost = false;
-    }, (error) => {
-      console.log(error);
-      this.isLoadingPost = false;
-    });
+    }, error => this.errorPostAction(error));
   }
 
   getComments(): void {
     this._commentApi.getItems(this.postId).subscribe((comments: IComment[]) => {
       this.comments = comments;
       this.isLoadingComments = false;
-    }, (error) => {
-      console.log(error);
-      this.isLoadingComments = false;
-    });
+    }, error => this.errorCommentAction(error));
   }
 
   editPost(): void {
@@ -80,9 +78,11 @@ export class SinglePostComponent implements OnInit {
       .afterClosed()
       .subscribe((editedPost): void => {
         if (editedPost) {
+          this.isLoadingPost = true;
           this._postApi.updateItem(editedPost).subscribe((updatedPost) => {
             this.post = updatedPost;
-          });
+            this.isLoadingPost = false;
+          }, error => this.errorPostAction(error));
         }
       });
   }
@@ -93,5 +93,43 @@ export class SinglePostComponent implements OnInit {
 
   showPostList() {
     this.showPosts.emit();
+  }
+
+  deleteComment(id: number): void {
+    this.dialog
+      .open(MessageDialogComponent, {
+        autoFocus: false,
+        disableClose: true,
+        data: {
+          title: 'Delete comment?',
+          type: MessageModalType.confirm,
+          buttonsNames: {
+            approve: 'Save',
+            decline: 'Cancel'
+          }
+        } as IMessageModal
+      })
+      .afterClosed()
+      .pipe(
+        filter((res: boolean): boolean => res)
+      )
+      .subscribe((editedPost): void => {
+        this.isLoadingComments= true;
+        this._commentApi.deleteItem(id).subscribe(() => {
+          const index = this.comments.findIndex(e => e.id === id);
+          this.comments.splice(index, 1);
+          this.isLoadingComments= false;
+        }, error => this.errorCommentAction(error));
+      });
+  }
+
+  errorCommentAction(error: Error): void {
+    console.log('Error: ', error);
+    this.isLoadingComments = false;
+  }
+
+  errorPostAction(error: Error): void {
+    console.log('Error: ', error);
+    this.isLoadingPost = false;
   }
 }
