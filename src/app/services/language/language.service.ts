c76480f6020f148872ebb5f12d96022250e9af77
Maksym Panchenko <panchenko.maksym@requestum.com>
2022-10-04 11:43:54 +0300

    feat: save current lang to local storage

---
+++ src/app/services/language/language.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService extends TranslateService {
+  saveCurrentLang(lang: string): void {
+    localStorage.setItem('lang', lang);
+  }
+
+  loadCurrentLang(): string | null {
+    return localStorage.getItem('lang');
+  }
+}
